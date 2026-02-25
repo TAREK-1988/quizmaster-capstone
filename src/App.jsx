d378d08863e7d92ce3aed8eec9745258a7368475
TAREK <tareksayedmokhtar@yahoo.com>
2026-02-25 21:10:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Layout from "./app/layout/layout.jsx";
+import Layout from "./app/Layout/layout.jsx";
 import AppRoutes from "./app/routes/AppRoutes.jsx";
 import { QuizProvider } from "./app/providers/QuizProvider.jsx";
 
